0160423ef4a777f69e5c4a9fe3c5a9654b38dbf9
Tiago Chaves <tiago.chaves@tempest.com.br>
2020-03-03 21:39:40 -0300

    First commit with project base.

---
+++ BeiraDoRio/Workers/CalculatorAPI.swift
@@ -0,0 +1,15 @@
+//
+//  CalculatorAPI.swift
+//  BeiraDoRio
+//
+//  Created by Tiago Chaves on 03/03/20.
+//  Copyright © 2020 Tempest. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorAPI {
+    func sum(_ value1:Double, with value2:Double) -> Double {
+        return value1 + value2
+    }
+}
---
+++ BeiraDoRioTests/Scenes/Sum/CalculatorAPITests.swift
@@ -0,0 +1,22 @@
+//
+//  CalculatorAPITests.swift
+//  BeiraDoRioTests
+//
+//  Created by Tiago Chaves on 03/03/20.
+//  Copyright © 2020 Tempest. All rights reserved.
+//
+@testable import BeiraDoRio
+import XCTest
+
+class CalculatorAPITests: XCTestCase {
+
+    var sut: CalculatorAPI!
+    
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}
